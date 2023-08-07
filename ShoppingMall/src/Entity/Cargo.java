f0ec566c5c339fc1b6aea3b28dc331f80e12582a
wakkpu <ho0524870@gmail.com>
2023-08-07 20:41:37 +0900

    cargo and admin pages

--- ShoppingMall/src/Entity/Cargo.java
+++ ShoppingMall/src/Entity/Cargo.java
@@ -1,5 +1,10 @@
 package Entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Cargo {
 	private Long cargoId;
 	private long itemId;
--- ShoppingMall/src/Entity/OrderSet.java
+++ ShoppingMall/src/Entity/OrderSet.java
@@ -2,6 +2,11 @@ package Entity;
 
 import java.time.LocalDateTime;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class OrderSet {
 	private Long orderSetId;
 	private Long consumerId;

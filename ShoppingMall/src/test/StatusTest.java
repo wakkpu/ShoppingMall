5a5adda592c75de1ec26bb5181ead38a939dd4bf
wakkpu <ho0524870@gmail.com>
2023-08-07 19:33:55 +0900

    disabled test codes

--- ShoppingMall/src/test/StatusTest.java
+++ ShoppingMall/src/test/StatusTest.java
@@ -1,50 +1,50 @@
-package test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-
-import Entity.Status;
-import repo.StatusRepository;
-
-public class StatusTest {
-
-	StatusRepository statusRepo = new StatusRepository();
-	
-	@Test
-	public void getStatusByName() {
-		try {
-			Long status = statusRepo.selectStatusIdByName("입고");
-			assertEquals(1, status);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	void selectAllStatus() {
-		List<Status> result = new ArrayList<>();
-		
-		try {
-			result = statusRepo.selectAll();
-			assertEquals(result.size(), 1);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void getAllStatus() {
-		try {
-			List<Status> result = statusRepo.selectAll();
-			assertEquals(1, result.size());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-}
+//package test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import Entity.Status;
+//import repo.StatusRepository;
+//
+//public class StatusTest {
+//
+//	StatusRepository statusRepo = new StatusRepository();
+//	
+//	@Test
+//	public void getStatusByName() {
+//		try {
+//			Long status = statusRepo.selectStatusIdByName("입고");
+//			assertEquals(1, status);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	@Test
+//	void selectAllStatus() {
+//		List<Status> result = new ArrayList<>();
+//		
+//		try {
+//			result = statusRepo.selectAll();
+//			assertEquals(result.size(), 1);
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	@Test
+//	public void getAllStatus() {
+//		try {
+//			List<Status> result = statusRepo.selectAll();
+//			assertEquals(1, result.size());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//}
